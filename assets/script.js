2832dd2e46ed1ed7f8f6390e1aec0642eac55b66
RfadnjdExt <raficanggih@gmail.com>
2022-09-26 15:13:43 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+function bukaTutupMenu() {
+    var x = document.getElementById("wmodal");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function tutupmenu() {
+    var x = document.getElementById("wmodal");
+    x.style.display = "none";
+}
+
+$(document).ready(function () {
+    $("select").select2({});
+})
